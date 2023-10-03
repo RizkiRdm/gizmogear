80ab630e04d6b8a4cf5fad1280e03fea340b7b4c
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-10-03 14:25:54 +0700

    14-(UPDATE)=mengganti logo dari navbar dan menambahkan class uppercase

--- src/components/Elements/navbar/Logo.tsx
+++ src/components/Elements/navbar/Logo.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 import React from "react";
 
 const Logo: React.FC = () => {
-  return <div className="text-2xl font-bold">
+  return <div className="text-2xl font-bold uppercase">
     <Link href={'/'}>
-      Soping
+      GizmoGear.
     </Link>
   </div>;
 };
