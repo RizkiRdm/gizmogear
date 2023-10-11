f3c4a3e6f7840109163d1274b20dd4a930e07b21
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-10-11 16:12:39 +0700

    03-(UPDATE)=menambah metode sugesti untuk pencarian

--- src/components/Elements/navbar/SearchInput.tsx
+++ src/components/Elements/navbar/SearchInput.tsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+const suggestions = ['Laptop', 'Smartphone', 'Keyboard', 'Mouse', 'Headphones'];
 
 const SearchInput: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    setShowSuggestions(term.length > 0); // Tampilkan saran jika ada input
+  };
+
+  const handleSuggestionClick = (suggestion: string) => {
+    setSearchTerm(suggestion);
+    setShowSuggestions(false);
+  };
+
   return (
-    <input
-      type="text"
-      placeholder="Search Product"
-      className="md:block px-2 py-1 bg-gray-700 rounded w-full mx-auto"
-    />
+    <div className="relative">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="Cari barang..."
+        className="border p-2 rounded w-64"
+      />
+      {showSuggestions && (
+        <motion.ul
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 10 }}
+          transition={{ duration: 0.2 }}
+          className="absolute bg-white w-64 shadow mt-2 py-1 rounded"
+        >
+          {suggestions
+            .filter((suggestion) => suggestion.toLowerCase().includes(searchTerm.toLowerCase()))
+            .map((suggestion, index) => (
+              <li
+                key={index}
+                className="cursor-pointer px-4 py-2 hover:bg-gray-200"
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion}
+              </li>
+            ))}
+        </motion.ul>
+      )}
+    </div>
   );
 };
 
