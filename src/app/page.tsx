fa57facbc909a6f3f98dda7d50867d2eba4ba222
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-10-11 16:11:47 +0700

    02-(UPDATE)=kembali kesemula

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,16 @@
-'use client'
-import React, { useState, useEffect } from "react";
-import Navbar from "@/components/Layouts/navbar/Navbar";
-import Carousel from "@/components/Layouts/CarouselHomepage/Carousel";
-import ProductCards from "@/components/Layouts/ProductCard/ProductCards";
-import image1 from '../../../public/wallhaven-gp26od.jpg';
-import SliderProductCardSettings from "@/components/Fragments/Carousel/SliderProductCardSettings";
-import Footer from "@/components/Layouts/Footer/Footer";
+import React from 'react'
+import HomePages from '@/components/Pages/HomePage';
+import { Metadata } from 'next';
 
-interface Product {
-  _id: number
-  images: any
-  title: string
-  category: string
-  price: number
-}
-
-const HomePage: React.FC = ({ }) => {
-
-  const [products, setProducts] = useState<Product[]>([])
-
-  // simulasi fetching data
-  useEffect(() => {
-    const fetchingData = async () => {
-      const data: Product[] = [
-        {
-          _id: 1,
-          category: 'console',
-          title: 'Product 1',
-          price: 100000,
-          images: image1,
-        },
-        {
-          _id: 2,
-          category: 'console',
-          title: 'Product 2',
-          price: 10,
-          images: image1,
-        },
-        {
-          _id: 3,
-          category: 'console',
-          title: 'Product 3',
-          price: 10,
-          images: image1,
-        },
-        {
-          _id: 4,
-          category: 'console',
-          title: 'Product 4',
-          price: 10,
-          images: image1,
-        },
-      ]
-      setProducts(data)
-    }
-    fetchingData()
-  }, [])
-  return (
-    <>
-      <main>
-        {/* navbar */}
-        <Navbar />
-
-        {/* slider product */}
-        <section className="mt-2 relative">
-          <Carousel />
-        </section>
-
-        {/* slider product */}
-        <section className="mt-5 relative">
-          <div className="mb-5">
-            <h2 className="text-2xl font-semibold ml-5 my-4">
-              Produk yang baru dirilis
-            </h2>
-            <SliderProductCardSettings>
-              {products.map((product) => (
-                <div key={product._id} >
-                  <ProductCards product={product} />
-                </div>
-              ))}
-            </SliderProductCardSettings>
-          </div>
-        </section>
-
-        <section>
-          <Footer />
-        </section>
-      </main>
-    </>
-  );
+export const metadata: Metadata = {
+  title: "GizmoGear",
+  description: "GizmoGear"
 };
 
-export default HomePage;
+const HomePage = () => {
+  return (
+    <HomePages />
+  )
+}
+
+export default HomePage
