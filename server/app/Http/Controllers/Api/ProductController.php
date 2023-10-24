3bc3a116a5950d1d7a0a2ec67de4a8efbce7df2c
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-10-24 22:24:49 +0700

    11-(FEAT)=membuat method STORE-LARAVEL

--- server/app/Http/Controllers/Api/ProductController.php
+++ server/app/Http/Controllers/Api/ProductController.php
@@ -15,4 +15,31 @@ class ProductController extends Controller
         $products = Product::all();
         return new ProductResource(true, 'list products', $products);
     }
+
+    // create data
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required',
+            'category' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Batasan ukuran maksimal 2MB
+        ]);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->extension();
+            $image->move(public_path('images'), $imageName);
+            $data['image'] = $imageName;
+        }
+
+        $product = Product::create($data);
+
+        if ($product) {
+            return new ProductResource(true, 'success', $product);
+        } else {
+            return response()->json(['message' => 'failed create data']);
+        }
+    }
 }
