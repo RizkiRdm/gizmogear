3d372b60b7222ff8c4fd36e3c09decceeb0cf03b
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-10-24 12:19:29 +0700

    06-(FEAT)=membuat function GET DATA-LARAVEL

--- server/app/Http/Controllers/Api/ProductController.php
+++ server/app/Http/Controllers/Api/ProductController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        // get all data
+        $products = Product::all();
+        return new ProductResource(true, 'list products', $products);
+    }
 }
