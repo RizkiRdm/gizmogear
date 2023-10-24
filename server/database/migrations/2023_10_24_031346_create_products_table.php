33a4367f93bb2519d29777eb04a99fe35a555ba7
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-10-24 10:27:04 +0700

    05-(FEAT)=membuat migrations-LARAVEL

--- server/database/migrations/2023_10_24_031346_create_products_table.php
+++ server/database/migrations/2023_10_24_031346_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('category', 50);
+            $table->text('description');
+            $table->integer('price');
+            $table->string('images');
             $table->timestamps();
         });
     }
