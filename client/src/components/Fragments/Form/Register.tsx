20ca4f1483b84629b2141c01db26bc581b7fdf8a
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-10-31 15:39:09 +0700

    buat supaya form register bekerja

--- client/src/components/Fragments/Form/Register.tsx
+++ client/src/components/Fragments/Form/Register.tsx
@@ -1,22 +1,57 @@
+'use client'
 import ButtonForm from '@/components/Elements/Form/ButtonForm'
-import InputFields from '@/components/Elements/Form/InputFields'
+import axios from 'axios'
 import React from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+interface IFormsInputs {
+    username: string
+    password: string
+}
 
 const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors }
+    } = useForm<IFormsInputs>()
+
+    const router = useRouter()
+    const onSubmit: SubmitHandler<IFormsInputs> = async (data?) => {
+        try {
+            await axios.post('http://localhost:8000/api/register', data)
+            router.push('/login')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <>
-            <div className="my-2">
-                <InputFields type={'text'} placeholder={'Fullname'} name={'fullname'} />
-            </div>
-            <div className="my-2">
-                <InputFields type={'text'} placeholder={'Username'} name={'username'} />
-            </div>
-            <div className="my-2">
-                <InputFields type={'password'} placeholder={'*******'} name={'password'} />
-            </div>
-            <div className="my-2">
-                <ButtonForm label='Register' />
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)} autoComplete='none'>
+                <div className="my-2">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        {...register('username', { required: 'Input your username' })}
+                        className='w-full p-3 bg-zinc-500 rounded-md text-white'
+                    />
+                    {errors.username && <p className='text-red-500 font-bold'>{errors.username.message}</p>}
+                </div >
+                <div className="my-2">
+                    <input
+                        type="password"
+                        placeholder="*********"
+                        {...register('password', { required: 'Input your password' })}
+                        className='w-full p-3 bg-zinc-500 rounded-md text-white'
+                    />
+                    {errors.password && <p className='text-red-500 font-bold'>{errors.password.message}</p>}
+                </div>
+                <div className="my-2">
+                    <ButtonForm label='Register' />
+                </div>
+            </form >
         </>
     )
 }
