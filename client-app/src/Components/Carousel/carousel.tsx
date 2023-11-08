b079a31865503539130c9049e474172a8487aab9
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-08 22:35:01 +0700

    (UPDATE)-update data carousel & add button prev & next carousel

--- client-app/src/Components/Carousel/carousel.tsx
+++ client-app/src/Components/Carousel/carousel.tsx
@@ -1,42 +1,82 @@
 import CarouselHero from './CarouselHero'
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Box, Flex, IconButton } from '@chakra-ui/react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
 
 // setting carousel
 const settings = {
-    dots: true,
+    dots: false,
+    arrows: false,
     infinite: true,
+    autoplay: true,
     speed: 500,
+    autoplaySpeed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
 };
 
 
-import image from "../../../public/wallhaven-dp3lrj.png"
-
 const items = [
     {
         slug: "product-1",
         title: "Product 1",
-        src: image,
-        price: 50,
+        src: 'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+        ,
+        description: "Product top number one and the best in the world and best on japan country and have oscar award from new york USA",
+    },
+    {
+        slug: "product-2",
+        title: "Product 2",
+        src: 'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+        ,
+        description: "Product top number one",
     },
 ]
 
-
 const carousel: React.FC = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [slides, setSlides] = useState<Slider | null>(null)
     return (
         <>
-            <Slider {...settings}>
-                {items.map((product, index) => (
-                    <CarouselHero
-                        key={index}
-                        title={product.title}
-                        slug={product.slug}
-                        price={product.price}
-                        src={product.src}
+            <Box position={"relative"}>
+                <Slider {...settings} ref={(slides => setSlides(slides))}>
+                    {items.map((product, index) => (
+                        <CarouselHero
+                            key={index}
+                            title={product.title}
+                            slug={product.slug}
+                            description={product.description}
+                            src={product.src}
+                        />
+                    ))}
+                </Slider>
+
+                <Flex
+                    position="absolute"
+                    top="50%"
+                    transform="translateY(-50%)"
+                    width="full"
+                    justifyContent="space-between"
+                    px={4}
+                >
+                    <IconButton
+                        aria-label="Previous"
+                        icon={<ChevronLeftIcon />}
+                        fontSize="2xl"
+                        onClick={() => { slides?.slickPrev() } /* Fungsi untuk navigasi ke slide sebelumnya */}
                     />
-                ))}
-            </Slider>
+                    <IconButton
+                        aria-label="Next"
+                        icon={<ChevronRightIcon />}
+                        fontSize="2xl"
+                        onClick={() => { slides?.slickNext() } /* Fungsi untuk navigasi ke slide berikutnya */}
+                    />
+                </Flex>
+            </Box>
+
         </>
     )
 }
