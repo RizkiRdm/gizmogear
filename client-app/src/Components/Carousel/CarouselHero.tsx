505622679d9b6dd44747a96cb4525d358b72927b
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-08 22:35:35 +0700

    (UPDATE)-change ui

--- client-app/src/Components/Carousel/CarouselHero.tsx
+++ client-app/src/Components/Carousel/CarouselHero.tsx
@@ -1,51 +1,55 @@
-import { Box, Link as ChakraLink, Image, Text } from "@chakra-ui/react";
+import { Box, Link as ChakraLink, Flex, Image, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 
 interface bannerProps {
     title: string
+    description: string
     slug: string
-    price: number
     src: string
 }
 
-const CarouselHero: React.FC<bannerProps> = ({ slug, title, src, price }) => {
+const CarouselHero: React.FC<bannerProps> = ({ slug, title, src, description }) => {
+    const descripttionProduct = description.split(" ").slice(0, 10).join(" ") + ("...")
     return (
-        <Box as='div' position='relative' width='full' height='96' overflow='hidden'>
+        <Flex
+            direction={{ base: "column", md: "row" }} // Flex direction column for mobile and tablet, row for larger screens
+            justify="center" // Center horizontally
+            align="center" // Center vertically
+            position="relative"
+            width="100%" // Take up full width of the parent
+            height="100vh" // Set a minimum height to cover the whole viewport
+        >
             {/* image hero */}
             <Image
                 src={src}
                 alt={title}
-                objectFit='cover'
+                objectFit="cover"
+                width="100%" // Ensure the image takes up full width
+                height="100%" // Ensure the image takes up full height
             />
 
-            <Box position='absolute' bottom='2' left='2' padding='6' backgroundColor='blackAlpha.500'>
+            <Box
+                position="absolute"
+                padding="8"
+                backgroundColor="blackAlpha.500"
+                borderRadius="8px"
+                textAlign="center"
+            >
                 {/* title product */}
-                <Text color='white' fontSize='xl' fontWeight='bold'>
+                <Text color="white" fontSize={{ base: "xl", md: "2xl" }} fontWeight="bold">
                     {title}
                 </Text>
-                {/* price product */}
-                <Text color='gray.800' fontSize='xl' fontWeight='bold'>
-                    {price}
+
+                <Text color="white" fontSize={{ base: "xl", md: "2xl" }} fontWeight="bold" my={"2"}>
+                    {descripttionProduct}
                 </Text>
                 {/* button/link */}
-                <ChakraLink as={Link} to={`/products/${slug}`} textDecoration='none'>
-                    <Box
-                        as='button'
-                        backgroundColor='teal.500'
-                        color='white'
-                        fontSize='md'
-                        fontWeight='semibold'
-                        py='2'
-                        px='4'
-                        borderRadius='md'
-                        transition='all 0.2s'
-                        _hover={{ backgroundColor: 'teal.600' }}
-                    >
-                        View Details
-                    </Box>
+                <ChakraLink as={Link} to={`/products/${slug}`} textDecoration="none" mt={{ base: 4, md: 0 }}>
+                    <Button label="View Details" />
                 </ChakraLink>
             </Box>
-        </Box>
+        </Flex>
     );
 };
 
