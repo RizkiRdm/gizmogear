753cd12764cdd76591a1a870d3fb7d2be3db1bb1
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-10 13:25:35 +0700

    (FEAT)-add package react query

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -5,14 +5,24 @@ import App from './Routes/Route'
 import "./index.css"
 import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
+import {
+  QueryClient,
+  QueryClientProvider
+} from "react-query"
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider>
-        <RecoilRoot>
-          <App />
-        </RecoilRoot>
-      </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <RecoilRoot>
+            <App />
+          </RecoilRoot>
+        </ChakraProvider>
+        <ReactQueryDevtools />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode >,
 )
