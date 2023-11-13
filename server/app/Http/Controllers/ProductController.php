82769739ce3b950a276b9f6283c150e2383493b9
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-13 23:58:50 +0700

    hapus request

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductResource;
 use Carbon\Carbon;
