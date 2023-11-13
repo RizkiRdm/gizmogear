23e5f32ba66a2c758af9f6a288117a7a20355e79
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-13 22:27:46 +0700

    (FEAT)-make atom for fetching state three products

--- client-app/src/Recoil/atom.ts
+++ client-app/src/Recoil/atom.ts
@@ -4,7 +4,23 @@ interface searchState {
     title: string
     slug: string
 }
+
+export interface ProductProps {
+    id: number;
+    title: string;
+    description: string;
+    slug: string;
+    image: string;
+    price: number;
+}
+
+
 export const searchResultState = atom({
     key: 'search-sugestion',
     default: [] as searchState[]
 })
+
+export const ThreeProductState = atom<ProductProps[]>({
+    key: 'ThreeProductState',
+    default: [],
+});
