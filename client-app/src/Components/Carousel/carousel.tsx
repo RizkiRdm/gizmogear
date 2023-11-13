5337c24bf1a3ba7f2f370eca8aef0388d808852b
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-13 22:21:03 +0700

    (UPDATE)-using recoil for state management

--- client-app/src/Components/Carousel/carousel.tsx
+++ client-app/src/Components/Carousel/carousel.tsx
@@ -5,6 +5,8 @@ import "slick-carousel/slick/slick-theme.css";
 import { Box, Flex, IconButton } from '@chakra-ui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { ThreeProductState } from '../../Recoil/atom';
 
 // setting carousel
 const settings = {
@@ -18,39 +20,17 @@ const settings = {
     slidesToScroll: 1,
 };
 
-
-const items = [
-    {
-        slug: "product-1",
-        title: "Product 1",
-        src: 'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
-        ,
-        description: "Product top number one and the best in the world and best on japan country and have oscar award from new york USA",
-    },
-    {
-        slug: "product-2",
-        title: "Product 2",
-        src: 'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
-        ,
-        description: "Product top number one",
-    },
-]
-
 const carousel: React.FC = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [slides, setSlides] = useState<Slider | null>(null)
+    const threeProduct = useRecoilValue(ThreeProductState)
+
     return (
         <>
             <Box position={"relative"}>
                 <Slider {...settings} ref={(slides => setSlides(slides))}>
-                    {items.map((product, index) => (
-                        <CarouselHero
-                            key={index}
-                            title={product.title}
-                            slug={product.slug}
-                            description={product.description}
-                            src={product.src}
-                        />
+                    {threeProduct.map((product) => (
+                        <CarouselHero products={product} />
                     ))}
                 </Slider>
 
