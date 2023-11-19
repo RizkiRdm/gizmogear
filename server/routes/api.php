f4a0779b85f9693f3c5810cf9bc1eb7922a69885
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-19 19:17:18 +0700

    (UPDATE)-make CRUD process just admin can have access

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
@@ -22,15 +21,15 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 // Rute CRUD produk hanya bisa diakses oleh admin
-// Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
-// });
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/', [ProductController::class, 'store']); // Endpoint untuk membuat produk baru
+    Route::put('/{product}', [ProductController::class, 'update']); // Endpoint untuk mengupdate produk
+    Route::delete('/{product}', [ProductController::class, 'destroy']); // Endpoint untuk menghapus produk
+});
 
 Route::prefix('products')->group(function () {
     Route::get('/', [ProductController::class, 'index']); // Endpoint untuk mendapatkan semua produk
     Route::get('/product/{slug}', [ProductController::class, 'show']); // Endpoint untuk mendapatkan detail produk
-    Route::post('/', [ProductController::class, 'store']); // Endpoint untuk membuat produk baru
-    Route::put('/{product}', [ProductController::class, 'update']); // Endpoint untuk mengupdate produk
-    Route::delete('/{product}', [ProductController::class, 'destroy']); // Endpoint untuk menghapus produk
     Route::get('/search', [ProductController::class, 'search']); // Endpoint untuk mencari produk berdasarkan nama
     Route::get('/latest', [ProductController::class, 'getLatestProduct']); // Endpoint untuk mendapatkan produk terbaru
     Route::get('/random-six-product', [ProductController::class, 'getSixProduct']); // Endpoint untuk mendapatkan 6 produk secara acak
