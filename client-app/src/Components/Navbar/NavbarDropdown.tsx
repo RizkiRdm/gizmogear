45929b31b43a575a3ff1ec8552297778fa9d4b5b
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-21 22:23:33 +0700

    (FEAT)-make label can get username & role

--- client-app/src/Components/Navbar/NavbarDropdown.tsx
+++ client-app/src/Components/Navbar/NavbarDropdown.tsx
@@ -1,11 +1,21 @@
 import { Button, Menu, MenuButton, Text } from "@chakra-ui/react"
+import { useRecoilState } from "recoil"
+import { isLoggedInState, roleState, usernameState } from "../../Recoil/atom"
 
 interface navbarDropdownProps {
     children: React.ReactNode
-    label: string
 }
 
-const NavbarDropdown: React.FC<navbarDropdownProps> = ({ children, label }) => {
+const NavbarDropdown: React.FC<navbarDropdownProps> = ({ children }) => {
+    const [getUsername] = useRecoilState(usernameState)
+    const [isLoggedIn] = useRecoilState(isLoggedInState)
+    const [isRole] = useRecoilState(roleState)
+    let label: string | JSX.Element = 'Hello User'
+
+    if (isLoggedIn) {
+        label = isRole === 'admin' ? `${getUsername}(ADMIN)` : getUsername
+    }
+
     return (
         <Menu>
             <MenuButton
@@ -15,7 +25,9 @@ const NavbarDropdown: React.FC<navbarDropdownProps> = ({ children, label }) => {
                 cursor={'pointer'}
                 minW={0}
             >
-                <Text as={'p'} fontSize='inherit' color={"gray.100"}>{label}</Text>
+                <Text as={'p'} fontSize='inherit' color={"gray.100"}>
+                    {label}
+                </Text>
             </MenuButton>
             {children}
         </Menu>
