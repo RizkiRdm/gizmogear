5824d63176d3af9ca08e584f80f4e3d373985315
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-24 00:11:47 +0700

    (UPDATE)-change bg & text color

--- client-app/src/Components/Sidebar/SidebarContent.tsx
+++ client-app/src/Components/Sidebar/SidebarContent.tsx
@@ -3,7 +3,6 @@ import { IconType } from 'react-icons';
 import { FiHome } from "react-icons/fi";
 import NavItem from "./NavItem";
 
-
 interface SidebarProps {
     onClose: () => void;
 }
@@ -22,12 +21,13 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
     return (
         <Box
             transition="3s ease"
-            bg={useColorModeValue('white', 'gray.900')}
+            bg={'gray.900'}
             borderRight="1px"
             borderRightColor={useColorModeValue('gray.200', 'gray.700')}
             w={{ base: 'full', md: 60 }}
             pos="fixed"
             h="full"
+            textColor={'whitesmoke'}
             {...rest}>
             <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
                 <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
