aad12649b203f83d4a21494b21e9a39621c74fdd
Rizki Romdhoni <rizkiromdhoni28@gmail.com>
2023-11-24 00:11:24 +0700

    (FEAT)-add logout function

--- client-app/src/Components/Sidebar/MobileNav.tsx
+++ client-app/src/Components/Sidebar/MobileNav.tsx
@@ -1,18 +1,34 @@
-import { Avatar, Box, Flex, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList, Text, VStack, useColorModeValue } from "@chakra-ui/react"
-import { FiBell, FiChevronDown, FiMenu } from "react-icons/fi"
+import { Box, Flex, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList, Text, VStack, useColorModeValue } from "@chakra-ui/react"
+import { FiChevronDown, FiMenu } from "react-icons/fi"
+import SearchInput from "../Navbar/NavbarSearch"
+import { useNavigate } from "react-router-dom"
 
 interface MobileProps {
     onOpen: () => void
 }
 
 const MobileNav = ({ onOpen }: MobileProps) => {
+    // function logout
+    const navigate = useNavigate()
+    const handleLogout = () => {
+
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+        localStorage.removeItem('role');
+
+        window.location.reload()
+        setTimeout(() => {
+            navigate('/')
+        }, 200);
+    }
+
     return (
         <Flex
             ml={{ base: 0, md: 60 }}
             px={{ base: 4, md: 4 }}
             height="20"
             alignItems="center"
-            bg={useColorModeValue('white', 'gray.900')}
+            bg={'gray.900'}
             borderBottomWidth="1px"
             borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
             justifyContent={{ base: 'space-between', md: 'flex-end' }}
@@ -29,29 +45,29 @@ const MobileNav = ({ onOpen }: MobileProps) => {
                 display={{ base: 'flex', md: 'none' }}
                 fontSize="2xl"
                 fontFamily="monospace"
-                fontWeight="bold">
-                Logo
+                fontWeight="bold"
+                color={'white'}
+            >
+                GizmoGear
             </Text>
 
             <HStack spacing={{ base: '0', md: '6' }}>
-                <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
+                <Flex align={'center'} justify={'space-evenly'} textColor={'white'}>
+                    <SearchInput />
+                </Flex>
                 <Flex alignItems={'center'}>
                     <Menu>
                         <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
                             <HStack>
-                                <Avatar
-                                    size={'sm'}
-                                    src={
-                                        'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                    }
-                                />
                                 <VStack
                                     display={{ base: 'none', md: 'flex' }}
                                     alignItems="flex-start"
                                     spacing="1px"
-                                    ml="2">
+                                    ml="2"
+                                    textColor={'white'}
+                                >
                                     <Text fontSize="sm">Justina Clark</Text>
-                                    <Text fontSize="xs" color="gray.600">
+                                    <Text fontSize="xs" color="gray.300">
                                         Admin
                                     </Text>
                                 </VStack>
@@ -63,7 +79,9 @@ const MobileNav = ({ onOpen }: MobileProps) => {
                         <MenuList
                             bg={useColorModeValue('white', 'gray.900')}
                             borderColor={useColorModeValue('gray.200', 'gray.700')}>
-                            <MenuItem>Sign out</MenuItem>
+                            <form onClick={handleLogout}>
+                                <MenuItem>Sign out</MenuItem>
+                            </form>
                         </MenuList>
                     </Menu>
                 </Flex>
