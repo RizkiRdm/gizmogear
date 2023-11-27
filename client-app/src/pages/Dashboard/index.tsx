e085821015451d0b7337c6bbd3e39b7d836642cd
RizkiRdm <rizkiromdhoni28@gmail.com>
2023-11-27 23:36:42 +0700

    (WO)-make card for knowing amount items & users

--- client-app/src/pages/Dashboard/index.tsx
+++ client-app/src/pages/Dashboard/index.tsx
@@ -1,9 +1,102 @@
+import { Badge, Box, Card, CardBody, Flex, Heading, Stack, Text } from "@chakra-ui/react"
 import Sidebar from "../../Components/Sidebar/Sidebar"
 
 const Dashboard = () => {
     return (
         <>
-            <Sidebar />
+            <Sidebar>
+                {/* content dashboard */}
+                <Box ml={{ base: 0, md: 60 }} p="4">
+                    <Box id="#">
+                        <Flex justify={'space-between'} gap={'4'}>
+                            <Card
+                                direction={{ base: 'column', sm: 'row' }}
+                                overflow='hidden'
+                                variant='outline'
+                                w={'50%'}
+                            >
+                                <Stack>
+                                    <CardBody>
+                                        <Heading>Jumlah user</Heading>
+                                        <Stack direction={'row'}>
+                                            <Badge colorScheme="green">
+                                                <Text fontSize={'md'}>100</Text>
+                                            </Badge>
+                                            <Badge colorScheme="gray">
+                                                <Text fontSize={'md'}>user</Text>
+                                            </Badge>
+                                        </Stack>
+                                    </CardBody>
+                                </Stack>
+                            </Card>
+                            <Card
+                                direction={{ base: 'column', sm: 'row' }}
+                                overflow='hidden'
+                                variant='outline'
+                                w={'50%'}
+                            >
+                                <Stack>
+                                    <CardBody>
+                                        <Heading>Jumlah barang</Heading>
+                                        <Stack direction={'row'}>
+                                            <Badge colorScheme="green">
+                                                <Text fontSize={'md'}>100</Text>
+                                            </Badge>
+                                            <Badge colorScheme="gray">
+                                                <Text fontSize={'md'}>barang</Text>
+                                            </Badge>
+                                        </Stack>
+                                    </CardBody>
+                                </Stack>
+                            </Card>
+                        </Flex>
+                    </Box>
+                    <Box id="#products">
+                        <Flex justify={'space-between'} gap={'4'}>
+                            <Card
+                                direction={{ base: 'column', sm: 'row' }}
+                                overflow='hidden'
+                                variant='outline'
+                                w={'50%'}
+                            >
+                                <Stack>
+                                    <CardBody>
+                                        <Heading>Jumlah user</Heading>
+                                        <Stack direction={'row'}>
+                                            <Badge colorScheme="green">
+                                                <Text fontSize={'md'}>100</Text>
+                                            </Badge>
+                                            <Badge colorScheme="gray">
+                                                <Text fontSize={'md'}>user</Text>
+                                            </Badge>
+                                        </Stack>
+                                    </CardBody>
+                                </Stack>
+                            </Card>
+                            <Card
+                                direction={{ base: 'column', sm: 'row' }}
+                                overflow='hidden'
+                                variant='outline'
+                                w={'50%'}
+                            >
+                                <Stack>
+                                    <CardBody>
+                                        <Heading>Jumlah barang</Heading>
+                                        <Stack direction={'row'}>
+                                            <Badge colorScheme="green">
+                                                <Text fontSize={'md'}>100</Text>
+                                            </Badge>
+                                            <Badge colorScheme="gray">
+                                                <Text fontSize={'md'}>barang</Text>
+                                            </Badge>
+                                        </Stack>
+                                    </CardBody>
+                                </Stack>
+                            </Card>
+                        </Flex>
+                    </Box>
+                </Box>
+            </Sidebar>
         </>
     )
 }
