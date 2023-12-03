55231a6cf788967cfdc02ba72b3f6b6f8daca921
RizkiRdm <rizkiromdhoni28@gmail.com>
2023-12-03 23:52:15 +0700

    (UPDATE)-make crud is universal

--- server/routes/api.php
+++ server/routes/api.php
@@ -20,14 +20,14 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-// Rute CRUD produk hanya bisa diakses oleh admin
-Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
-    Route::post('/', [ProductController::class, 'store']); // Endpoint untuk membuat produk baru
-    Route::put('/{product}', [ProductController::class, 'update']); // Endpoint untuk mengupdate produk
-    Route::delete('/{product}', [ProductController::class, 'destroy']); // Endpoint untuk menghapus produk
-});
 
 Route::prefix('products')->group(function () {
+
+    // Rute CRUD produk hanya bisa diakses oleh admin
+    Route::post('/create', [ProductController::class, 'store']); // Endpoint untuk membuat produk baru
+    Route::put('/edit/{product}', [ProductController::class, 'update']); // Endpoint untuk mengupdate produk
+    Route::delete('/delete/{product}', [ProductController::class, 'destroy']); // Endpoint untuk menghapus produk
+
     Route::get('/', [ProductController::class, 'index']); // Endpoint untuk mendapatkan semua produk
     Route::get('/product/{slug}', [ProductController::class, 'show']); // Endpoint untuk mendapatkan detail produk
     Route::get('/search', [ProductController::class, 'search']); // Endpoint untuk mencari produk berdasarkan nama
