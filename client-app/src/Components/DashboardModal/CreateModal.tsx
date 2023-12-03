c5f89e03a77fbe769d3c5f3d4cf52a85aba3260f
RizkiRdm <rizkiromdhoni28@gmail.com>
2023-12-03 23:50:24 +0700

    (UPDATE)-make filter, search, and create new product

--- client-app/src/Components/DashboardModal/CreateModal.tsx
+++ client-app/src/Components/DashboardModal/CreateModal.tsx
@@ -11,13 +11,16 @@ import {
     ModalFooter,
     ModalHeader,
     ModalOverlay,
-    Textarea
+    Textarea,
+    useToast
 } from "@chakra-ui/react";
-import { SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { createProduct } from "../../api/api";
 
 interface InputProps {
     title: string;
-    image: string;
+    image: FileList;
     category: string;
     description: string;
     price: number;
@@ -25,60 +28,133 @@ interface InputProps {
 
 interface CreateModalProps {
     onOpen: boolean;
-    onClose: () => void;
-    onSubmit: SubmitHandler<InputProps>;
-    register: any; // Tipe bisa disesuaikan dengan tipe register yang digunakan pada useForm
-    errors: any; // Tipe bisa disesuaikan dengan tipe errors yang digunakan pada useForm
+    onClose: () => void
 }
 
-const CreateModal: React.FC<CreateModalProps> = ({ onOpen, onClose, onSubmit, register, errors }) => {
+const CreateModal: React.FC<CreateModalProps> = ({ onOpen, onClose }) => {
+    const [isSubmitting, setIsSubmitting] = useState(false) // State untuk mengatur status submit
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        setValue
+    } = useForm()
+
+    const handleImageChange = (e: any) => {
+        if (e.target.files && e.target.files[0]) {
+            setValue('image', e.target.files);
+        }
+    };
+    const toast = useToast()
+    const onSubmitForm: SubmitHandler<InputProps> = async (data) => {
+        try {
+            setIsSubmitting(true)
+            const formData = new FormData();
+            formData.append('title', data.title);
+            formData.append('category', data.category);
+            formData.append('description', data.description);
+            formData.append('price', data.price);
+            formData.append('image', data.image[0]);
+            const res = await createProduct(formData)
+            if (res) {
+                toast({
+                    position: 'top-right',
+                    title: `success create new product`,
+                    status: 'success',
+                    duration: 2000,
+                    isClosable: false,
+                })
+                setTimeout(() => {
+                    setIsSubmitting(false)
+                    onClose()
+                }, 2000);
+            } else {
+                toast({
+                    position: 'top-right',
+                    title: `Failed create product`,
+                    status: 'error',
+                    duration: 2000,
+                    isClosable: false,
+                })
+            }
+        } catch (error) {
+            setIsSubmitting(false)
+            console.error(error)
+        }
+
+
+    }
     return (
         <>
             <Modal isOpen={onOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <form onSubmit={onSubmit}>
+                    <form onSubmit={handleSubmit(onSubmitForm)}>
                         <ModalHeader>Create new product</ModalHeader>
                         <ModalCloseButton />
                         <ModalBody pb={6}>
                             <FormControl isRequired>
                                 <FormLabel>Title Product</FormLabel>
                                 <Input placeholder='Title Product' {...register('title', {
-                                    required: 'Enter title product'
+                                    required: true
                                 })} />
-                                <FormErrorMessage>{errors.title && errors.title.message}</FormErrorMessage>
+                                {errors.title?.type === 'required' && (
+                                    <FormErrorMessage>
+                                        Title is required
+                                    </FormErrorMessage>
+                                )}
                             </FormControl>
 
-                            <FormControl isRequired>
+                            <FormControl isRequired my={3}>
                                 <FormLabel>Category Product</FormLabel>
+
                                 <Input placeholder='Category Product' {...register('category', {
-                                    required: 'Enter category product'
+                                    required: true
                                 })} />
-                                <FormErrorMessage>{errors.category && errors.category.message}</FormErrorMessage>
+                                {errors.category?.type === 'required' && (
+                                    <FormErrorMessage>
+                                        category is required
+                                    </FormErrorMessage>
+                                )}
                             </FormControl>
 
                             <FormControl isRequired>
                                 <FormLabel>Description Product</FormLabel>
                                 <Textarea placeholder='Description Product' {...register('description', {
-                                    required: 'Enter description product'
+                                    required: true
                                 })} />
-                                <FormErrorMessage>{errors.description && errors.description.message}</FormErrorMessage>
+                                {errors.description?.type === 'required' && (
+                                    <FormErrorMessage>
+                                        description is required
+                                    </FormErrorMessage>
+                                )}
                             </FormControl>
 
-                            <FormControl isRequired>
+                            <FormControl isRequired my={3}>
                                 <FormLabel>Price Product</FormLabel>
-                                <Input type="number" placeholder='Price Product' {...register('price', {
-                                    required: 'Enter price product'
-                                })} />
-                                <FormErrorMessage>{errors.price && errors.price.message}</FormErrorMessage>
+                                <Input type="number" {...register("price", {
+                                    required: true
+                                })}
+                                    placeholder="Price"
+                                />
+                                {errors.price?.type === 'required' && (
+                                    <FormErrorMessage>
+                                        category is required
+                                    </FormErrorMessage>
+                                )}
                             </FormControl>
 
                             <FormControl isRequired>
                                 <FormLabel>Image Product</FormLabel>
-                                <Input type="file" placeholder='Image Product' {...register('image', {
-                                    required: 'Upload image product'
-                                })} />
-                                <FormErrorMessage>{errors.image && errors.image.message}</FormErrorMessage>
+                                <Input
+                                    type="file"
+                                    onChange={handleImageChange}
+                                    {...register('image', {
+                                        required: true, // Menambahkan pesan error jika file tidak dipilih
+                                    })}
+                                    accept="image/*"
+                                />
                             </FormControl>
                         </ModalBody>
 
--- client-app/src/Components/DashboardTable/ProductTable.tsx
+++ client-app/src/Components/DashboardTable/ProductTable.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import {
     Button,
     Flex,
@@ -15,19 +14,11 @@ import {
     Tr,
     useDisclosure
 } from "@chakra-ui/react";
-import { SetStateAction, useState } from "react";
+import { useMemo, useState } from "react";
 import { useQuery } from "react-query";
 import { fetchAllProduct } from "../../api/api";
 import CreateModal from "../DashboardModal/CreateModal";
-import { SubmitHandler, useForm } from "react-hook-form";
-
-interface inputProps {
-    title: string
-    image: string
-    category: string
-    description: string
-    price: number
-}
+import EditProductModal from "../DashboardModal/EditModal";
 
 const ProductTable = () => {
     const { data: products, isLoading, isError } = useQuery('fetchProduct', fetchAllProduct);
@@ -36,55 +27,60 @@ const ProductTable = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 10;
 
-    if (isLoading) return <p>Loading...</p>;
-    if (isError) return <p>Error fetching data</p>;
-
-    let filteredProducts = [...products];
-
-    // Filter by search term
-    if (searchTerm) {
-        filteredProducts = filteredProducts.filter(product =>
-            product.title.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-    }
-
-    // Sort by price ASC or DESC
-    filteredProducts = filteredProducts.sort((a, b) => {
-        const priceA = a.price;
-        const priceB = b.price;
-
-        if (sortType === 'asc') {
-            return priceA - priceB;
-        } else {
-            return priceB - priceA;
-        }
-    });
-
-    // pagination item
-    const indexLastItem = currentPage * itemsPerPage
-    const indexFirstItem = currentPage - itemsPerPage
-    const currentItems = filteredProducts.slice(indexFirstItem, indexLastItem)
-
-    const paginate = (pageNumber: SetStateAction<number>) => setCurrentPage(pageNumber)
-
     // State untuk mengatur modal Create
     const {
         isOpen: isCreateOpen,
         onOpen: onCreateOpen,
-        onClose: onCreateClose,
+        onClose: onCreateClose
     } = useDisclosure();
 
+    // show modal for create
     const handleShowModal = () => {
         onCreateOpen();
     };
 
-    // Penggunaan useForm disini
-    const { register, handleSubmit, formState: { errors } } = useForm<inputProps>();
+    // show modal for edit
+    // State untuk mengatur modal Create
+    const {
+        isOpen: isEditOpen,
+        onOpen: onEditOpen,
+        onClose: onEditClose
+    } = useDisclosure();
 
-    const onCreateSubmit: SubmitHandler<inputProps> = (data) => {
-        console.log(data);
-        onCreateClose();
+    const handleShowEditModal = () => {
+        onCreateOpen();
     };
+    const filteredProducts = useMemo(() => {
+        if (!products) return [];
+
+        let filtered = [...products];
+
+        // Filter by search term
+        if (searchTerm) {
+            filtered = filtered.filter(product =>
+                product.title.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+        }
+
+        // Sort by price ASC or DESC
+        return filtered.sort((a, b) => {
+            const priceA: number | string = a.price;
+            const priceB: number | string = b.price;
+
+            return sortType === 'asc' ? priceA - priceB : priceB - priceA;
+        });
+    }, [products, searchTerm, sortType]);
+    // pagination item
+    const indexLastItem = currentPage * itemsPerPage;
+    const indexFirstItem = currentPage - itemsPerPage;
+    const currentItems = filteredProducts.slice(indexFirstItem, indexLastItem);
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+
+    if (isLoading) return <p>Loading...</p>;
+    if (isError) return <p>Error fetching data</p>;
+
     return (
         <>
             <HStack>
@@ -97,6 +93,7 @@ const ProductTable = () => {
                 <Button colorScheme="blue" onClick={handleShowModal}>Create New Product</Button>
             </HStack>
             {filteredProducts.length === 0 && <Text>No products found</Text>}
+            {/* Render Table */}
             <TableContainer>
                 <Table>
                     <Thead>
@@ -123,7 +120,7 @@ const ProductTable = () => {
                             <Tr key={product.id}>
                                 <Td>{index + 1}</Td>
                                 <Td>
-                                    <Image src={product.image} boxSize={'20rem'} w={'100%'} objectFit={'cover'} />
+                                    <Image src={product.image} w={'100%'} height={"50%"} objectFit={'cover'} />
                                 </Td>
                                 <Td>{product.title}</Td>
                                 <Td>{product.category}</Td>
@@ -131,7 +128,7 @@ const ProductTable = () => {
                                 <Td>{product.price}</Td>
                                 <Td>
                                     <Flex justify='space-evenly' align={'center'} gap={5}>
-                                        <Button colorScheme="yellow">Edit</Button>
+                                        <Button colorScheme="yellow" onClick={handleShowEditModal}>Edit</Button>
                                         <Button colorScheme="red">Delete</Button>
                                     </Flex>
                                 </Td>
@@ -154,12 +151,11 @@ const ProductTable = () => {
             <CreateModal
                 onOpen={isCreateOpen}
                 onClose={onCreateClose}
-                onSubmit={handleSubmit(onCreateSubmit)}
-                register={register}
-                errors={errors}
             />
+
+            <EditProductModal isOpen={onEditOpen} onClose={onEditClose} />
         </>
-    )
-}
+    );
+};
 
 export default ProductTable;
